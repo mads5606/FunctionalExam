71e1d1f96dcc9ecabde2615e642a23a2df259746
Daniel Skjold Toft <dstoft@hotmail.com>
2021-03-30 09:37:48 +0200

    Deleted node_modules and updated .gitignore

--- node_modules/fast-check/lib/esm/check/arbitrary/helpers/ArrayHelper.js
+++
@@ -1,8 +0,0 @@
-export function findOrUndefined(ts, f) {
-    for (var i = 0; i < ts.length; i++) {
-        var t = ts[i];
-        if (f(t))
-            return t;
-    }
-    return undefined;
-}
--- node_modules/fast-check/lib/ts3.2/check/arbitrary/helpers/ArrayHelper.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @hidden
- * Find first element matching the predicate in the array, or null if none match
- * Equivalent to Array.prototype.find, but works on Internet Explorer 11.
- */
-export declare function findOrUndefined<T>(ts: ArrayLike<T>, f: (t: T) => boolean): T | undefined;
