95bb106aea60dddbeaf35f25b8835e9f5abbb1a9
Daniel Skjold Toft <dstoft@hotmail.com>
2021-05-06 12:41:21 +0200

    The baseTest.ts will now start docker containers, run the tests and then stop the containers again This allows for multiple runs of the tests itself

--- test/baseTest.ts
+++ test/baseTest.ts
@@ -8,6 +8,19 @@ const server = {
     port: 8085,
 };
 
+// https://stackoverflow.com/questions/1880198/how-to-execute-shell-command-in-javascript
+const execSync = require('child_process').execSync;
+
+// https://stackoverflow.com/questions/16873323/javascript-sleep-wait-before-continuing
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
 const validUsers = [100, 101, 102];
 
 // describe("GET Test", () => {
@@ -38,11 +51,22 @@ const validUsers = [100, 101, 102];
 
 
 test('model test', async () => {
+    const outputUp = execSync('docker-compose up -d --force-recreate', {encoding: 'utf-8'});  // the default is 'buffer'
+    // console.log('Output up was:\n', outputUp);
+    sleep(5000);
+    let i = 1;
     await fc.assert(
         fc.asyncProperty(fc.commands(OrbitCommands, 100), async (cmds) => {
-            const s = () => ({model: new OrbitModel(), real: new OrbitImpl(validUsers)});
+            const s = () => ({
+                model: new OrbitModel(),
+                real: new OrbitImpl(validUsers, {host: "localhost", port: (18080 + i)})
+            });
             await fc.asyncModelRun(s, cmds);
+            i++;
         }),
-        {numRuns: 1, endOnFailure: true, verbose: true}
+        {numRuns: 3, endOnFailure: true, verbose: true}
     );
+    const outputDown = execSync('docker-compose down', {encoding: 'utf-8'});  // the default is 'buffer'
+    // console.log('Output down was:\n', outputDown);
+    sleep(5000);
 }, 20000);
